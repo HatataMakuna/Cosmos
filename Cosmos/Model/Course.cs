e8d670fbae54ddcecf69396da230d17b7dcadbd7
._. <128780634+HatataMakuna@users.noreply.github.com>
2025-08-25 17:19:50 +0800

    initial implementation for manage courses

---
+++ Cosmos/Model/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Model
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Stage> Stages { get; set; }
+
+        public Course(int iD, string name, List<Stage> stages)
+        {
+            ID = iD;
+            Name = name;
+            Stages = stages;
+        }
+    }
+}
---
+++ Cosmos/Model/Stage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Model
+{
+    public class Stage
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public List<Obstacle> Obstacles { get; set; }
+        public double TimeLimit { get; set; } // in seconds
+
+        public Stage(string iD, string name, List<Obstacle> obstacles, double timeLimit)
+        {
+            ID = iD;
+            Name = name;
+            Obstacles = obstacles;
+            TimeLimit = timeLimit;
+        }
+    }
+}
